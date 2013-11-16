558f000dab7df077febf05ecd4e5b6c7bde6ffc1
Ryan Rix <ry@n.rix.si>
2013-11-16 15:46:01 -0800

    Modernize existing roles, port to roles-based layout and make-work

---
+++ roles/spiff/templates/spiff-local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'spiff',
+        'USER': 'spiff',
+        'PASSWORD': '{{spiff_postgresql_password}}',
+        'HOST': '',
+        'PORT': '',
+    }
+}
